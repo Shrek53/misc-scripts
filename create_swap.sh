5f7d284b42722a213ac693ea77cff1b9f3b5b67a
apon <apon.ruet@gmail.com>
2024-10-14 15:37:08 +0600

    feat: make the swap file persistant

--- create_swap.sh
+++ create_swap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+echo "This script creates a swap file for your machine."
 echo "swap_size = block_size X number_of_blocks"
-read -p "Block size : (i.e. 256M,512M,1G,2G )" bs
-read -p "Number of blocks : " cnt
+read -p "Enter Block size : (i.e. 256M,512M,1G,2G )" bs
+read -p "Enter Number of blocks : " cnt
 sudo swapoff -a
 sudo dd if=/dev/zero of=/swapfile bs=$bs count=$cnt
 sudo mkswap /swapfile
@@ -10,3 +11,6 @@ sudo swapoff -a
 sudo swapon /swapfile
 echo "Your swap size : "
 grep SwapTotal /proc/meminfo
+echo "Making swapfile permanent : "
+sudo cp /etc/fstab /etc/fstab.back
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
